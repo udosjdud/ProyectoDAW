e18182036297e6514feb9747bb1c0a90d22d7639
udosjdud <lixinyinpa25@gmail.com>
2025-05-04 13:59:03 +0200

    Actualización de la bbdd

--- proyecto_daw.sql
+++ proyecto_daw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-04-2025 a las 19:49:13
+-- Tiempo de generación: 04-05-2025 a las 13:57:59
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,11 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `espacios_trabajos` (
-  `id` bigint(20) UNSIGNED NOT NULL,
+  `id` smallint(20) UNSIGNED NOT NULL,
   `titulo` varchar(255) NOT NULL,
-  `propietario` int(11) NOT NULL,
+  `id_propietario` smallint(20) UNSIGNED NOT NULL,
   `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
-  `background_img` text DEFAULT NULL
+  `background_img` text DEFAULT 'NB'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lista`
+--
+
+CREATE TABLE `lista` (
+  `id` smallint(20) UNSIGNED NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `id_espacio_trabajo` smallint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -42,7 +54,7 @@ CREATE TABLE `espacios_trabajos` (
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` smallint(20) UNSIGNED NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `passw` blob NOT NULL,
@@ -54,7 +66,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `passw`, `foto_perfil`) VALUES
-(1, 'Lixin', 'lixinyinpa25@gmail.com', 0x9091a98cc32754db3d1863dc29439c07, 'nd');
+(1, 'Probador', 'pruebas@gmail.com', 0x2432792431302451686d616a50714e414a6c4f7438353751397857462e365437392e746e5372344f6d7a73676c5459352f6558524f3267524754316d, 'nd');
 
 --
 -- Índices para tablas volcadas
@@ -65,7 +77,14 @@ INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `passw`, `foto_perfil`) VALUES
 --
 ALTER TABLE `espacios_trabajos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_propietario` (`propietario`);
+  ADD KEY `fk_propietario` (`id_propietario`);
+
+--
+-- Indices de la tabla `lista`
+--
+ALTER TABLE `lista`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_espacio_trabajo` (`id_espacio_trabajo`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -82,13 +101,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `espacios_trabajos`
 --
 ALTER TABLE `espacios_trabajos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` smallint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `lista`
+--
+ALTER TABLE `lista`
+  MODIFY `id` smallint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` smallint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -98,7 +123,13 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `espacios_trabajos`
 --
 ALTER TABLE `espacios_trabajos`
-  ADD CONSTRAINT `fk_propietario` FOREIGN KEY (`propietario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `fk_propietario` FOREIGN KEY (`id_propietario`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `lista`
+--
+ALTER TABLE `lista`
+  ADD CONSTRAINT `lista_ibfk_1` FOREIGN KEY (`id_espacio_trabajo`) REFERENCES `espacios_trabajos` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
