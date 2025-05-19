0ede408d8b547625897968e151d424f86f304720
udosjdud <lixinyinpa25@gmail.com>
2025-05-19 19:11:28 +0200

    id de tablas

--- scripts/espacio_trabajo.js
+++ scripts/espacio_trabajo.js
@@ -21,7 +21,7 @@ function pintarTodasTablas() {
                     const fechaFormateado = fecha.toISOString().split('T')[0];
 
                     contentTables.append(`
-                        <div class='tabla'>
+                        <div class='tabla' data-tabla-id="${tablesData[i].id}">
                             <h4>${tablesData[i].titulo}</h4>
                             <p>Fecha de creación: ${fechaFormateado}</p>
                         </div>
