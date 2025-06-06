6684495ec8f5b2c7d85e9c9fbb5fc1f1507032e3
udosjdud <lixinyinpa25@gmail.com>
2025-06-06 16:09:03 +0200

    Cambiar title

--- paginas/aplicacion/tablero.php
+++ paginas/aplicacion/tablero.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Tablero</title>
+    <title>TaskMaster</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
